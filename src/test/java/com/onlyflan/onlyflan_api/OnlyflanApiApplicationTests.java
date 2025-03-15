71adcdfe7096e83332c0b9cb056c7f675bf98560
VictorBartolomeo <victor.monteragioni@icloud.com>
2025-03-15 16:55:56 +0100

    first commit

---
+++ src/test/java/com/onlyflan/onlyflan_api/OnlyflanApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlyflan.onlyflan_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnlyflanApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
